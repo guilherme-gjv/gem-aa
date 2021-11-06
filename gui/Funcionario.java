ca9d3b84b6729c3eec1e9cfdc3624d654ab772a0
guilherme-gjv <83377948+guilherme-gjv@users.noreply.github.com>
2021-11-06 00:03:14 -0300

    feat: principais classes criadas

---
+++ gui/Funcionario.java
@@ -0,0 +1,38 @@
+package gui;
+
+import java.util.*;
+
+public class Funcionario extends Pessoa{
+    private float salario;
+    private String setor;
+
+    private Scanner strLeitor = new Scanner(System.in);
+    private Scanner numLeitor = new Scanner(System.in);
+    
+    Funcionario(){
+        super();
+        setFuncionario();
+    }
+    private void setFuncionario() {
+        System.out.println("Salário: ");
+        salario = numLeitor.nextFloat();
+        System.out.println("Setor: ");
+        setor = strLeitor.nextLine();
+    }
+    void exibirFuncionario(){
+        exibir();
+        System.out.println("Salário: "+salario);
+        System.out.println("Setor: "+ setor);
+    }
+    public void updateFuncionario(){
+        updatePessoa();
+
+    }
+    public float getSalario(){
+        return salario;
+    }
+    public String getSetor(){
+        return setor;
+    }
+
+}
