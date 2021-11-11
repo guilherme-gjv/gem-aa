2a981b0120d00156a05e58b65e5af315a290c7d4
guilherme-gjv <83377948+guilherme-gjv@users.noreply.github.com>
2021-11-10 21:15:03 -0300

    init: allana-amanda

---
+++ allanaamanda/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        menu();
+    }
+    private static void menu(){
+        System.out.println("Distribuidora Nossa Arapiraca");
+        System.out.println("1: CADASTRAR FUNCIONARIO\n2: CADASTRAR E CONSULTAR CLIENTE\n3: EXIBIR GERENTES\n4: ATUALIZAR ENDEREÇO E TELEFONE DE FUNCIONÁRIO\n5: ");
+    }
+}
