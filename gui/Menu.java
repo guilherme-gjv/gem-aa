2d1e3d79e5b37fa980c0d3cf4008372aaf17110d
guilherme-gjv <83377948+guilherme-gjv@users.noreply.github.com>
2021-12-20 10:05:20 -0300

    feat: implementação da interface

--- gui/Menu.java
+++ gui/Menu.java
@@ -4,14 +4,19 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 import gui.Pessoas.*;
+import gui.Produtos.*;
 
-public class Menu {
+public class Menu implements IEstoque {
     Scanner strLeitor = new Scanner(System.in);
     Scanner numLeitor = new Scanner(System.in);
 
     ArrayList<Vendedor> vendedor = new ArrayList<>();
     ArrayList<Gerente> gerente = new ArrayList<>();
     ArrayList<Pessoa> cliente = new ArrayList<>();
+
+    public ArrayList<Produto> produtos = new ArrayList<>();
+    public ArrayList<ProdutoPerecivel> produtosPereciveis = new ArrayList<>();
+    public ArrayList<Lampada> lampadas = new ArrayList<>();
 
     public Menu() {
 
@@ -20,36 +25,36 @@ public class Menu {
             exibirMenu();
             resposta = numLeitor.nextInt();
             switch (resposta) {
-            case 1:
-                venda();
-                break;
-            case 2:
-                cadastroFuncionario();
-                break;
-            case 3:
-                cadastroCliente();
-                break;
-            case 4:
-                consultaCliente();
-                break;
-            case 5:
-                exibirGerentes();
-                break;
-            case 6:
-                updateVendedor();
-                break;
-            case 7:
-                updateGerente();
-                break;
-            case 8:
-                updateCliente();
-                break;
-            case 9:
-                exibirDados();
-                break;
-            case 10:
-                System.out.println("Saindo...");
-                break;
+                case 1:
+                    venda();// método venda PRECISA ser atualizado.
+                    break;
+                case 2:
+                    cadastroFuncionario();
+                    break;
+                case 3:
+                    cadastroCliente();
+                    break;
+                case 4:
+                    consultaCliente();
+                    break;
+                case 5:
+                    exibirGerentes();
+                    break;
+                case 6:
+                    updateVendedor();
+                    break;
+                case 7:
+                    updateGerente();
+                    break;
+                case 8:
+                    updateCliente();
+                    break;
+                case 9:
+                    exibirDados();
+                    break;
+                case 10:
+                    System.out.println("Saindo...");
+                    break;
             }
         } while (resposta != 10);
 
@@ -76,7 +81,7 @@ public class Menu {
         if (vendedor.isEmpty()) {
             System.out.println("Cadastre um Vendedor antes de fazer uma venda! ");
         } else {
-            vendedor.get(pesquisa(1)).novaVenda();
+            vendedor.get(pesquisaPessoa(1)).novaVenda();
         }
 
     }
@@ -86,14 +91,14 @@ public class Menu {
         System.out.println("Digite o tipo: \n1 - Vendedor;\n2 - Gerente;");
         tipo = numLeitor.nextInt();
         switch (tipo) {
-        case 1:
-            Vendedor v = new Vendedor();
-            vendedor.add(v);
-            break;
-        case 2:
-            Gerente g = new Gerente();
-            gerente.add(g);
-            break;
+            case 1:
+                Vendedor v = new Vendedor();
+                vendedor.add(v);
+                break;
+            case 2:
+                Gerente g = new Gerente();
+                gerente.add(g);
+                break;
         }
     }
 
@@ -104,7 +109,7 @@ public class Menu {
 
     private void consultaCliente() {
         System.out.println("Pesquisar Cliente");
-        int i = pesquisa(3);
+        int i = pesquisaPessoa(3);
         if (i == -1) {
             System.out.println("Não encontrado! ");
         } else {
@@ -123,7 +128,7 @@ public class Menu {
 
     private void updateVendedor() {
         System.out.println("Atualizar Vendedor ");
-        int i = pesquisa(1);
+        int i = pesquisaPessoa(1);
         if (i == -1) {
             System.out.println("Não encontrado! ");
         } else {
@@ -133,9 +138,9 @@ public class Menu {
     }
 
     private void updateGerente() {
-        
+
         System.out.println("Atualizar Gerente ");
-        int i = pesquisa(2);
+        int i = pesquisaPessoa(2);
         if (i == -1) {
             System.out.println("Não encontrado! ");
         } else {
@@ -146,7 +151,7 @@ public class Menu {
 
     private void updateCliente() {
         System.out.println("Atualizar Cliente ");
-        int i = pesquisa(3);
+        int i = pesquisaPessoa(3);
         if (i == -1) {
             System.out.println("Não encontrado! ");
         } else {
@@ -162,79 +167,79 @@ public class Menu {
         System.out.println("3 - Uma pessoa específica");
         escolha = numLeitor.nextInt();
         switch (escolha) {
-        case 1:
-            for (int i = 0; i < vendedor.size(); i++) {
-                vendedor.get(i).exibirVendedor();
-            }
-
-            exibirGerentes();
-
-            for (int i = 0; i < cliente.size(); i++) {
-                cliente.get(i).exibir();
-            }
-            break;
-        case 2:
-            tipo = 0;
-            System.out.println("Digite o tipo: \n1 - Vendedor;\n2 - Gerente;\n3 - Cliente; ");
-            tipo = numLeitor.nextInt();
-            switch (tipo) {
             case 1:
                 for (int i = 0; i < vendedor.size(); i++) {
                     vendedor.get(i).exibirVendedor();
                 }
-                break;
-            case 2:
-                for (int i = 0; i < gerente.size(); i++) {
-                    gerente.get(i).exibirGerente();
-                }
-                break;
-            case 3:
+
+                exibirGerentes();
+
                 for (int i = 0; i < cliente.size(); i++) {
                     cliente.get(i).exibir();
                 }
                 break;
-            }
-            break;
-        case 3:
-            tipo = 0;
-            System.out.println("Digite o tipo: \n1 - Vendedor;\n2 - Gerente;\n3 - Cliente; ");
-            tipo = numLeitor.nextInt();
-            int i = 0;
-            switch (tipo) {
-            case 1:
-                i = pesquisa(1);
-                if (i == -1) {
-                    System.out.println("Não encontrado!");
-                } else {
-                    vendedor.get(i).exibirVendedor();
-                }
-
-                break;
             case 2:
-                i = pesquisa(2);
-                if (i == -1) {
-                    System.out.println("Não encontrado!");
-                } else {
-                    gerente.get(i).exibirGerente();
+                tipo = 0;
+                System.out.println("Digite o tipo: \n1 - Vendedor;\n2 - Gerente;\n3 - Cliente; ");
+                tipo = numLeitor.nextInt();
+                switch (tipo) {
+                    case 1:
+                        for (int i = 0; i < vendedor.size(); i++) {
+                            vendedor.get(i).exibirVendedor();
+                        }
+                        break;
+                    case 2:
+                        for (int i = 0; i < gerente.size(); i++) {
+                            gerente.get(i).exibirGerente();
+                        }
+                        break;
+                    case 3:
+                        for (int i = 0; i < cliente.size(); i++) {
+                            cliente.get(i).exibir();
+                        }
+                        break;
                 }
-
                 break;
             case 3:
-                consultaCliente();
+                tipo = 0;
+                System.out.println("Digite o tipo: \n1 - Vendedor;\n2 - Gerente;\n3 - Cliente; ");
+                tipo = numLeitor.nextInt();
+                int i = 0;
+                switch (tipo) {
+                    case 1:
+                        i = pesquisaPessoa(1);
+                        if (i == -1) {
+                            System.out.println("Não encontrado!");
+                        } else {
+                            vendedor.get(i).exibirVendedor();
+                        }
+
+                        break;
+                    case 2:
+                        i = pesquisaPessoa(2);
+                        if (i == -1) {
+                            System.out.println("Não encontrado!");
+                        } else {
+                            gerente.get(i).exibirGerente();
+                        }
+
+                        break;
+                    case 3:
+                        consultaCliente();
+                        break;
+                    default:
+                        System.out.println("Tipo inválido! Digite 1, 2 ou 3");
+                        break;
+                }
                 break;
             default:
-                System.out.println("Tipo inválido! Digite 1, 2 ou 3");
+                System.out.println("Escolha inválida! Digite 1, 2 ou 3");
                 break;
-            }
-            break;
-        default:
-            System.out.println("Escolha inválida! Digite 1, 2 ou 3");
-            break;
         }
 
     }
 
-    public int pesquisa(int tipoDePessoa) {
+    public int pesquisaPessoa(int tipoDePessoa) {
         // Vendedor 1;
         // Gerente 2;
         // Cliente 3;
@@ -242,33 +247,140 @@ public class Menu {
         System.out.println("Digite o nome: ");
         nomePesquisa = strLeitor.nextLine();
         switch (tipoDePessoa) {
-        case 1:
-            for (int i = 0; i < vendedor.size(); i++) {
-                if (vendedor.get(i).getNome().equalsIgnoreCase(nomePesquisa)) {
-                    return i;
+            case 1:
+                for (int i = 0; i < vendedor.size(); i++) {
+                    if (vendedor.get(i).getNome().equalsIgnoreCase(nomePesquisa)) {
+                        return i;
+                    }
                 }
-            }
-            break;
-        case 2:
-            for (int i = 0; i < gerente.size(); i++) {
-                if (gerente.get(i).getNome().equalsIgnoreCase(nomePesquisa)) {
-                    return i;
+                break;
+            case 2:
+                for (int i = 0; i < gerente.size(); i++) {
+                    if (gerente.get(i).getNome().equalsIgnoreCase(nomePesquisa)) {
+                        return i;
+                    }
                 }
-            }
-            break;
-        case 3:
-            for (int i = 0; i < cliente.size(); i++) {
-                if (cliente.get(i).getNome().equalsIgnoreCase(nomePesquisa)) {
-                    return i;
+                break;
+            case 3:
+                for (int i = 0; i < cliente.size(); i++) {
+                    if (cliente.get(i).getNome().equalsIgnoreCase(nomePesquisa)) {
+                        return i;
+                    }
                 }
-            }
-            break;
-        default:
-            return -1;
+                break;
         }
 
         System.out.println("Nome não encontrado!");
 
+        return -1;
+    }
+
+    @Override
+    public void cadastrarProdutos() {
+        System.out.println("--Cadastrar produto--");
+        System.out.println("Tipo de Produto");
+        System.out.println("1 - Produto Simples ");
+        System.out.println("2 - Produto Perecível");
+        System.out.println("3 - Lâmpada");
+        System.out.println("4 - Sair ");
+        int escolha = numLeitor.nextInt();
+        switch (escolha) {
+            case 1:
+                Produto p = new Produto("Produto Simples");
+                produtos.add(p);
+                break;
+            case 2:
+                ProdutoPerecivel pp = new ProdutoPerecivel();
+                produtosPereciveis.add(pp);
+                break;
+            case 3:
+                Lampada l = new Lampada();
+                lampadas.add(l);
+                break;
+            default:
+                System.out.println("Cancelando cadastro...");
+                break;
+        }
+    }
+
+    @Override
+    public void updateProdutos() {
+        System.out.println("--Atualizar produtos--");
+        System.out.println("Tipo de Produto");
+        System.out.println("1 - Produto Simples ");
+        System.out.println("2 - Produto Perecível");
+        System.out.println("3 - Lâmpada");
+        System.out.println("4 - Sair ");
+        int escolha = numLeitor.nextInt();
+        int indice;
+        switch (escolha) {
+            case 1:
+                indice = pesquisaProduto(1);
+                produtos.get(indice).updateDados();
+                break;
+            case 2:
+                indice = pesquisaProduto(2);
+                produtosPereciveis.get(indice).updateProdutoPerecivel();
+                break;
+            case 3:
+                indice = pesquisaProduto(3);
+                lampadas.get(indice).updateLampada();
+                break;
+            default:
+                System.out.println("Cancelando cadastro...");
+                break;
+        }
+
+    }
+
+    @Override
+    public void consultarProdutos() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void deletarProdutos() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void removerProdutoForaDaValidade() {
+        // TODO Auto-generated method stub
+
+    }
+
+    private int pesquisaProduto(int tipodeDeProduto) {
+        // 1 - Produto Simples
+        // 2 - Produto Perecível
+        // 3 - Lâmpada
+        System.out.println("Digite o código do produto: ");//não coloquei código
+        int codigo = numLeitor.nextInt();
+        switch (tipodeDeProduto) {
+            case 1:
+                for(int i = 0; i < produtos.size(); i++ ){
+                    if(produtos.get(i).getCodigo() == codigo){
+                        return i;
+                    }
+                }
+                break;
+            case 2:
+                for(int i = 0; i < produtosPereciveis.size(); i++){
+                    if(produtosPereciveis.get(i).getCodigo() == codigo){
+                        return i;
+                    }
+                }
+                break;
+            case 3:
+                for(int i = 0; i < produtosPereciveis.size(); i++){
+                    if(lampadas.get(i).getCodigo() == codigo){
+                        return i;
+                    }
+                }
+                break;
+        }
+        System.out.println("Produto não encontrado! ");
         return -1;
     }
 
